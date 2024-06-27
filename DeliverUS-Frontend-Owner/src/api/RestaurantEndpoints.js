3be7a4eb47df86c1625f28aaa8a1f94f8e4ecdc4
Lidia Fernández <lidiaestudios14@gmail.com>
2024-06-27 22:09:23 +0200

    Initial commit

---
+++ DeliverUS-Frontend-Owner/src/api/RestaurantEndpoints.js
@@ -0,0 +1,31 @@
+import { get, post, put, destroy, patch } from './helpers/ApiRequestsHelper'
+function getAll () {
+  return get('users/myrestaurants')
+}
+
+function getDetail (id) {
+  return get(`restaurants/${id}`)
+}
+
+function getRestaurantCategories () {
+  return get('restaurantCategories')
+}
+
+function create (data) {
+  return post('restaurants', data)
+}
+
+function update (id, data) {
+  return put(`restaurants/${id}`, data)
+}
+
+function remove (id) {
+  return destroy(`restaurants/${id}`)
+}
+
+// solucion
+function actualizar (id) {
+  return patch(`restaurants/${id}`)
+}
+
+export { getAll, getDetail, getRestaurantCategories, create, update, remove, actualizar }
